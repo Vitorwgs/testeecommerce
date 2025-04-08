039ac501c74253b68f5f4d93f9e32f0b009b0677
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-04-07 21:15:10 -0300

    Add files via upload

--- proj_integrador-main/ecommerce/frontend/js/menu.js
+++ proj_integrador-main/ecommerce/frontend/js/menu.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menuPages = document.getElementById('menu-pages');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBar = document.querySelector('.menu_bar');
+    const menuPages = document.querySelector('.menu-pages');
 
-    if (menuToggle && menuPages) {
-        menuToggle.addEventListener('click', () => {
-            menuPages.classList.toggle('menu-open');
+    if (menuBar && menuPages) {
+        menuBar.addEventListener('click', function() {
+            menuPages.style.display = menuPages.style.display === 'block' ? 'none' : 'block';
         });
     }
 });
